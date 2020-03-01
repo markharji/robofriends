7099cbeff1445f1187b922c0bde51f87f5d10da7
markharji <47886260+markharji@users.noreply.github.com>
2020-03-01 18:16:20 +0800

    Updates

---
+++ precache-manifest.e5fbd7fa4f8961424be93e01bfe5e560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845fa7eb2fa226505f2bfb1140623216",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fb48f7907da1b91a456b",
+    "url": "/robofriends/static/css/main.457d605f.chunk.css"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/robofriends/static/js/2.5b396b40.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.5b396b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb48f7907da1b91a456b",
+    "url": "/robofriends/static/js/main.b3ed3736.chunk.js"
+  },
+  {
+    "revision": "5677d8ff5cb4c373cfaa",
+    "url": "/robofriends/static/js/runtime-main.c712519d.js"
+  }
+]);
